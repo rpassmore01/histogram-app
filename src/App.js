c1b8288a02ca6deeefa9dd87dc82fcbeaf7bc192
rpassmore01 <russellpassmore2005@gmail.com>
2021-10-27 17:08:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Jimp from 'jimp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Upload extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      file: null,
+      redValues: [],
+      blueValues: [],
+      greenValues: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.updateValue = this.updateValue.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    }, () => {this.updateValue();})
+  }
+  updateValue() {
+    Jimp.read(this.state.file, (err, img) => {
+      let localredValues = [];
+      let localgreenValues = [];
+      let localblueValues = [];
+      for (let i = 0; i < img.bitmap.width; i++){
+        for (let j = 0; j < img.bitmap.height; j++){
+          let hex = img.getPixelColor(i, j);
+          let RGBA = Jimp.intToRGBA(hex);
+          localredValues.push(RGBA.r)
+          localgreenValues.push(RGBA.g)
+          localblueValues.push(RGBA.b)
+        }
+      }
+      this.setState({
+        redValues: localredValues,
+        blueValues: localblueValues,
+        greenValues: localgreenValues
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input type="file" onChange={this.handleChange}/>
+        <img src={this.state.file} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Upload;
